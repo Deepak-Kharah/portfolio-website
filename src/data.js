398ed00059806e69290e0809a3892ef666ead71f
Deepak Kharah <kharahdeepak009@gmail.com>
2020-05-11 18:42:22 +0530

    data addition attempt

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const skills = [
+	{ title: 'languages', skills: [ 'Python', 'JavaScript', 'C++' ] },
+	{ title: 'framework', skills: [ 'Django', 'React' ] },
+	{ title: 'tools', skills: [ 'Git', 'GitHub', 'BitBucket' ] },
+	{ title: 'database', skills: [ 'MongoDb', 'PostgreSql', 'MySql' ] }
+];
+export const projects = [];
+export const mentions = [ { title: '', date: '', desc: '' } ];
